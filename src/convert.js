16686599d2b8e1714c45619e0a531ac3c466518b
Mateus Manuel <mateusmanuel@unb.br>
2019-01-07 13:44:07 -0200

    Fix error when catalog is not an array

--- src/convert.js
+++ src/convert.js
@@ -19,18 +19,21 @@ module.exports.convert = (options) => {
 
     let info = {name: "services-ems-bus", schema: "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"}
 
+    console.log('Processing files...\n');
+    
     results.forEach(filePath => {
-        console.log(filePath);
         const services = servicesFromFile(filePath);
 
-        if(services[0].catalog) return;
+        if(!Array.isArray(services) || services[0].catalog) return;
         
+        console.log(filePath);
         let catalogName = filePath.split('/').pop();
         catalogName = catalogName.replace('catalogo_', '').replace('.json', '');
 
         item.push({name: catalogName, item: itemFromService(services)});
     });
     
+    console.log('\nDone!');
     
     return JSON.stringify({info, item}, null, '\t');
 };
